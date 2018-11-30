5ea99776c989687f5be2502f8c2c505eed68f3f4
AlexS <alexandre.suchot@ynov.com>
2018-11-30 15:21:08 +0100

    Creation de la database avec 3 pokemons pour test et affichage sur le twig

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Pokemon;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
@@ -9,8 +10,38 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $charizard = new Pokemon();
+        $charizard
+            ->setName("Charizard")
+            ->setAttack(80)
+            ->setLife(120)
+            ->setDefense(60)
+            ->setSpeed(50)
+            ->setImage("https://archives.bulbagarden.net/media/upload/7/7e/006Charizard.png")
+            ->setType("Fire");
+        $manager->persist($charizard);
+
+        $blastoise = new Pokemon();
+        $blastoise
+            ->setName("Blastoise")
+            ->setAttack(60)
+            ->setLife(120)
+            ->setDefense(80)
+            ->setSpeed(35)
+            ->setImage("https://archives.bulbagarden.net/media/upload/0/02/009Blastoise.png")
+            ->setType("Water");
+        $manager->persist($blastoise);
+
+        $venusaur = new Pokemon();
+        $venusaur
+            ->setName("Venusaur")
+            ->setAttack(55)
+            ->setLife(130)
+            ->setDefense(70)
+            ->setSpeed(40)
+            ->setImage("https://archives.bulbagarden.net/media/upload/a/ae/003Venusaur.png")
+            ->setType("Grass");
+        $manager->persist($venusaur);
 
         $manager->flush();
     }
