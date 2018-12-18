fa9b863771ba63bcb8b26eace6736e092c863bf4
Unknown <robin.bigeard@gmail.com>
2018-12-18 10:48:21 +0100

    Admin user

--- src/Migrations/Version20181218080630.php
+++ src/Migrations/Version20181218080630.php
@@ -8,13 +8,14 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20181217122553 extends AbstractMigration
+final class Version20181218080630 extends AbstractMigration
 {
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
+        $this->addSql('CREATE TABLE pokemon (id INT AUTO_INCREMENT NOT NULL, number_pokedex INT NOT NULL, name VARCHAR(255) NOT NULL, type VARCHAR(255) NOT NULL, life INT NOT NULL, attack INT NOT NULL, defense INT NOT NULL, speed INT NOT NULL, image VARCHAR(255) NOT NULL, icon VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, pseudo VARCHAR(180) NOT NULL, email VARCHAR(180) NOT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:array)\', password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D64986CC499D (pseudo), UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
     }
 
@@ -23,6 +24,7 @@ final class Version20181217122553 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
+        $this->addSql('DROP TABLE pokemon');
         $this->addSql('DROP TABLE user');
     }
 }
