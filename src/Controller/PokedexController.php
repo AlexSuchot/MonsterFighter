1671660578862d444fd69d60b4663069ac21c1ad
Marceau David <marceau.david@ynov.com>
2018-12-19 15:43:30 +0100

    some css fixes

--- src/Controller/PokedexController.php
+++ src/Controller/PokedexController.php
@@ -8,7 +8,6 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\File\File as File;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
